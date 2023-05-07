37faec8a7231cc3e759e9d6d1fd1823da31855a0
RHLance <lance.endaya@rewardholdings.com>
2023-05-08 07:19:18 +0800

    chore: add exp test

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -4,20 +4,22 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Error {
     uint public num = 5;
-    
-    function testRequire(uint _i) public pure {
+
+    function testRequire(uint _i) public {
         require(_i == 5, "The value must be equal to 5");
+        num += 1;
     }
 
-    function testAssert() public view {
-        assert(num == 0);
+    function testAssert(uint _i) public {
+        assert(_i == 0);
+        num += 1;
     }
 
-    function testCustomError(uint _withdrawAmount, address _userAdress) public view {
+    function testCustomError(uint _withdrawAmount, address _userAdress) public {
         uint bal = _userAdress.balance;
         if (bal < _withdrawAmount) {
             revert("Insufficient balance");
         }
+        num += 1;
     }
-
 }
